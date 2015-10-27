9b38afe9c3a7a522a6f7ba8e125b1b747698b183
Rakesh <ry2294@columbia.edu>
2015-10-27 16:55:02 -0400

    remove this change when pushed

--- app/dynamoDB.js
+++ app/dynamoDB.js
@@ -2,8 +2,8 @@ var AWS = require('aws-sdk');
 var _ = require('underscore');
 
 AWS.config.update({
-    accessKeyId: '', 
-    secretAccessKey: ''});
+    accessKeyId: 'AKIAJREVNX3K2ENDJP7Q', 
+    secretAccessKey: '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'});
 
 AWS.config.update({region: 'us-west-2'});
 
